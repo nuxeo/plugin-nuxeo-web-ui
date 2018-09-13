ac186bcd90a4184236e15dde9202f4212b153caa
Nuno Cunha <ncunha@nuxeo.com>
2018-09-13 10:48:12 +0100

    NXP-25773: Add functional tests to recently viewed documents.

--- nuxeo-web-ui-ftest/test/features/step_definitions/recents.js
+++ nuxeo-web-ui-ftest/test/features/step_definitions/recents.js
@@ -1,6 +1,28 @@
 'use strict';
 
 module.exports = function () {
-  this.Then('I can see the list of recently viewed documents',
-      () => this.ui.drawer.recents.waitForVisible().should.be.true);
+  this.Given(/^I have a clean recently viewed documents list/, () => {
+    fixtures.documents.clearLocalStorage(this.username, 'nuxeo-recent-documents');
+    fixtures.documents.reloadLocalStorage('* >>> nuxeo-recent-documents >>> #storage');
+  });
+
+  this.Given(/^I have document with path "(.+)" on the recently viewed documents list/, (path) => {
+    fixtures.documents.addToLocalStorage(path, this.username, 'nuxeo-recent-documents');
+  });
+
+  this.Then('I can see the list of recently viewed documents', () => {
+    this.ui.drawer.recents.waitForVisible().should.be.true;
+  });
+
+  this.Then('I can see the list of recently viewed documents has "$nb" items', (nb) => {
+    fixtures.documents.reloadLocalStorage('* >>> nuxeo-recent-documents >>> #storage');
+    this.ui.drawer.recents.waitForVisible();
+    this.ui.drawer.recents.nbItems.should.be.equals(parseInt(nb));
+  });
+
+  this.Then('I can see the list of recently viewed documents has "$title" document', (title) => {
+    fixtures.documents.reloadLocalStorage('* >>> nuxeo-recent-documents >>> #storage');
+    this.ui.drawer.recents.waitForVisible();
+    this.ui.drawer.recents.el.hasElementByTextContent('#recentDocumentsList .list-item-title', title).should.be.true;
+  });
 };
--- nuxeo-web-ui-ftest/test/pages/ui/clipboard.js
+++ nuxeo-web-ui-ftest/test/pages/ui/clipboard.js
@@ -6,7 +6,7 @@ export default class Clipboard extends BasePage {
 
   get nbItems() {
     // we need to force the clipboard storage reload since we may have by-passed it in the fixture
-    browser.execute(() => document.querySelector('* >>> nuxeo-clipboard >>> #storage').reload());
+    fixtures.documents.reloadLocalStorage('* >>> nuxeo-clipboard >>> #storage');
     const items = this.el.elements(`#list .list-item`).value;
     let count = 0;
     items.forEach((item) => {
