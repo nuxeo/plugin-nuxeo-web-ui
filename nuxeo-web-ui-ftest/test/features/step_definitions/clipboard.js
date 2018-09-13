5e620fbb4742ed672584904a9b1b3c10388745d1
Nuno Cunha <ncunha@nuxeo.com>
2018-09-13 10:48:12 +0100

    NXP-25773: Add functional tests to recently viewed documents.

--- nuxeo-web-ui-ftest/test/features/step_definitions/clipboard.js
+++ nuxeo-web-ui-ftest/test/features/step_definitions/clipboard.js
@@ -1,9 +1,26 @@
 'use strict';
 
 module.exports = function () {
-  this.Then('I can see the clipboard has "$nb" items', (nb) => {
+  this.Given(/^I have a clean clipboard$/, () => {
+    fixtures.documents.clearLocalStorage(this.username, 'nuxeo-clipboard');
+  });
+
+  this.Given(/^I have document with path "(.+)" on clipboard$/, (path) => {
+    fixtures.documents.addToLocalStorage(path, this.username, 'nuxeo-clipboard')
+        .then(() => this.ui.drawer.clipboard.reload());
+  });
+
+  this.Given(/^I have the following documents on clipboard$/, (table) => {
+    const promises = [];
+    table.rows().map((row) => {
+      promises.push(fixtures.documents.addToLocalStorage(row[0], this.username, 'nuxeo-clipboard'));
+    });
+    return Promise.all(promises).then(() => this.ui.drawer.clipboard.reload());
+  });
+
+  this.When('I click remove button for "$title" document', (title) => {
     this.ui.drawer.clipboard.waitForVisible();
-    this.ui.drawer.clipboard.nbItems.should.be.equals(parseInt(nb));
+    this.ui.drawer.clipboard.removeItem(title);
   });
   this.When('I click the clipboard move action', () => {
     if (!this.ui.drawer.clipboard.isVisible()) {
@@ -18,5 +35,14 @@ module.exports = function () {
     }
     this.ui.waitForToastNotVisible();
     this.ui.drawer.clipboard.paste();
+  });
+
+  this.Then('I can see the clipboard has "$title" document', (title) => {
+    this.ui.drawer.clipboard.waitForVisible();
+    this.ui.drawer.clipboard.el.hasElementByTextContent('#list .list-item-title', title).should.be.true;
+  });
+  this.Then('I can see the clipboard has "$nb" items', (nb) => {
+    this.ui.drawer.clipboard.waitForVisible();
+    this.ui.drawer.clipboard.nbItems.should.be.equals(parseInt(nb));
   });
 };
