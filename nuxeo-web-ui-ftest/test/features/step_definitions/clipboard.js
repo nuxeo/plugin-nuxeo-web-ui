e54f58e2a8cc12c6940b39f0bea328c9887e4272
Gabriel Barata <gbarata@nuxeo.com>
2018-09-26 15:21:36 +0100

    NXP-25057: refactor local storage handling

--- nuxeo-web-ui-ftest/test/features/step_definitions/clipboard.js
+++ nuxeo-web-ui-ftest/test/features/step_definitions/clipboard.js
@@ -1,22 +1,6 @@
 'use strict';
 
 module.exports = function () {
-  this.Given(/^I have a clean clipboard$/, () => {
-    fixtures.documents.clearLocalStorage(this.username, 'nuxeo-clipboard');
-  });
-
-  this.Given(/^I have document with path "(.+)" on clipboard$/, (path) => {
-    fixtures.documents.addToLocalStorage(path, this.username, 'nuxeo-clipboard')
-        .then(() => this.ui.drawer.clipboard.reload());
-  });
-
-  this.Given(/^I have the following documents on clipboard$/, (table) => {
-    const promises = [];
-    table.rows().map((row) => {
-      promises.push(fixtures.documents.addToLocalStorage(row[0], this.username, 'nuxeo-clipboard'));
-    });
-    return Promise.all(promises).then(() => this.ui.drawer.clipboard.reload());
-  });
 
   this.When('I click remove button for "$title" document', (title) => {
     this.ui.drawer.clipboard.waitForVisible();
@@ -39,10 +23,10 @@ module.exports = function () {
 
   this.Then('I can see the clipboard has "$title" document', (title) => {
     this.ui.drawer.clipboard.waitForVisible();
-    this.ui.drawer.clipboard.el.hasElementByTextContent('#list .list-item-title', title).should.be.true;
+    driver.waitUntil(() => this.ui.drawer.clipboard.el.hasElementByTextContent('#list .list-item-title', title));
   });
   this.Then('I can see the clipboard has "$nb" items', (nb) => {
     this.ui.drawer.clipboard.waitForVisible();
-    this.ui.drawer.clipboard.nbItems.should.be.equals(parseInt(nb));
+    driver.waitUntil(() => this.ui.drawer.clipboard.nbItems === parseInt(nb));
   });
 };
