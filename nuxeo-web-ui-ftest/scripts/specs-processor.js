ca3936555bfcb0f8b4f213e1f0b38a85c3630ad1
michael <mvachette@nuxeo.com>
2020-07-15 10:35:28 +0900

    NXP-29329: fix features location parsing

--- nuxeo-web-ui-ftest/scripts/specs-processor.js
+++ nuxeo-web-ui-ftest/scripts/specs-processor.js
@@ -5,7 +5,7 @@ module.exports = (argv) => {
   const args = require('minimist')(argv.slice(2));
 
   const separator = args.specs.lastIndexOf('/');
-  const files = Finder.from(args.specs.substring(0, separator)).findFiles(args.specs.substring(separator));
+  const files = Finder.from(args.specs.substring(0, separator)).findFiles(args.specs.substring(separator + 1));
   /*
    * XXX
    * tagExpression is being used only for syntax purposes, since we are not evaluating expressions.
