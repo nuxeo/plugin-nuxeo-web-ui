8e1b1c2844435cafb9e4ffe498ab72f6d0c075f1
Gabriel Barata <gbarata@nuxeo.com>
2022-02-03 11:57:47 +0000

    retry login step

--- nuxeo-web-ui-ftest/test/features/step_definitions/login.js
+++ nuxeo-web-ui-ftest/test/features/step_definitions/login.js
@@ -30,6 +30,7 @@ Given('user {string} exists', username => fixtures.users.create(
 ));
 
 When('I login as {string}', function (username) {
+When('I login as {string}', { wrapperOptions: { retry: 3 } }, function (username) {
   browser.pause(1000);
   const login = Login.get();
   login.username = username;
